9d644802bdc7e43be6cf8d890250ffde4ef96a5d
tishamaharjan <139973058+tishamaharjan@users.noreply.github.com>
2025-07-11 15:59:10 +0545

    fix(frontend): login register validation

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,33 +1,96 @@
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   const navigate = useNavigate();
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    navigate("/home");
+  const onSubmit = (data: FormData) => {
+    try {
+      const userKey = `user_${data.email}`;
+      const userDetails = localStorage.getItem(userKey);
+
+      if (!userDetails) {
+        alert("User not registered.");
+      } else {
+        if (userDetails) {
+          const user = JSON.parse(userDetails);
+          if (user.password === data.password && user.email === data.email) {
+            navigate("/home");
+          } else if (user.password !== data.password) {
+            alert("Invalid  password");
+          } else if (user.email !== data.email) {
+            alert("Invalid  email");
+          } else {
+            alert("Invalid email or password");
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Invalid email or password");
+    }
   };
   return (
     <div className="flex flex-col justify-center text-center">
       Login
       <div className="flex flex-col mt-5">
         <form
-          onSubmit={onSubmit}
-          className="bg-[#EAEFEF] border-2 w-1/3 mx-auto p-5 rounded-[10px]"
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-[#EAEFEF] flex flex-col gap-1 border-2 w-1/3 mx-auto p-5 rounded-[10px]"
         >
-          <label>Email:</label>
-          <input
-            type="text"
-            placeholder="Email"
-            className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
-            required
-          />
-          <label>Password:</label>
-          <input
-            type="password"
-            placeholder="Password"
-            className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
-            required
-          />
+          <>
+            <label>Email:</label>
+            <input
+              type="text"
+              placeholder="Email"
+              className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
+              {...register("email", {
+                required: true,
+              })}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-xs">Email is required</span>
+            )}
+          </>
+
+          <>
+            <label>Password:</label>
+            <div className="border-1 flex items-center rounded-[5px] w-[95%] focus-within:border-2 bg-white">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="px-1 rounded-[5px] w-full bg-transparent outline-none"
+                {...register("password", {
+                  required: true,
+                })}
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                color="gray"
+                className="cursor-pointer mr-1"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+
+            {errors.password && (
+              <span className="text-red-500 text-xs">Password is required</span>
+            )}
+          </>
           <button
             type="submit"
             className="bg-[#A7C1A8] text-white p-1 rounded-[5px] mt-3 w-full cursor-pointer"
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,61 +1,163 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+type FormData = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const schema = z
+  .object({
+    username: z.string().min(1, { message: "Username is required" }),
+    email: z
+      .string()
+      .min(1, { message: "Email is required" })
+      .regex(emailRegex, { message: "Invalid email" }),
+    password: z
+      .string()
+      .min(5, { message: "Password must be at least 5 characters long" }),
+    confirmPassword: z.string().min(1, { message: "Password should match" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const navigate = useNavigate();
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    localStorage.setItem(
-      "user_" + username,
-      JSON.stringify({ username, email, password })
-    );
-    navigate("/");
+  const onSubmit = (data: FormData) => {
+    try {
+      const result = schema.safeParse({
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      });
+      if (!result.success) {
+        result.error.issues.map((issue) => console.log(issue.message));
+        alert("Invalid email or password");
+      } else {
+        localStorage.setItem(
+          "user_" + data.email,
+          JSON.stringify({
+            username: data.username,
+            email: data.email,
+            password: data.password,
+          })
+        );
+        navigate("/");
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div className="flex flex-col justify-center text-center">
       Register
       <div className="flex flex-col  mt-5">
         <form
-          onSubmit={onSubmit}
-          className="bg-[#EAEFEF] border-2 w-1/3 mx-auto p-5 rounded-[10px]"
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-[#EAEFEF] flex flex-col gap-1 border-2 w-1/3 mx-auto p-5 rounded-[10px]"
         >
-          <label>Username:</label>
-          <input
-            type="text"
-            placeholder="Username"
-            className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-          <label>Email:</label>
-          <input
-            type="text"
-            placeholder="Email"
-            className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label>Password:</label>
-          <input
-            type="password"
-            placeholder="Password"
-            className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button
-            type="submit"
-            className="bg-[#A7C1A8] text-white p-1 rounded-[5px] mt-3 w-full cursor-pointer"
-          >
-            Register
-          </button>
+          <>
+            <label>Username:</label>
+            <input
+              type="text"
+              placeholder="Username"
+              {...register("username")}
+              className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
+            />
+            {errors.username && (
+              <span className="text-red-500 text-xs">
+                {errors.username.message}
+              </span>
+            )}
+          </>
+
+          <>
+            <label>Email:</label>
+            <input
+              type="text"
+              placeholder="Email"
+              {...register("email")}
+              className="border-1 px-1 rounded-[5px] w-[95%] bg-white"
+            />
+            {errors.email && (
+              <span className="text-red-500 text-xs">
+                {errors.email.message}
+              </span>
+            )}
+          </>
+
+          <>
+            <label>Password:</label>
+            <div className="border-1 flex items-center rounded-[5px] w-[95%] focus-within:border-2 bg-white">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="px-1 rounded-[5px] w-full bg-transparent outline-none"
+                {...register("password")}
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                color="gray"
+                className="cursor-pointer mr-1"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+            {errors.password && (
+              <span className="text-red-500 text-xs">
+                {errors.password.message}
+              </span>
+            )}
+          </>
+
+          <>
+            <label>Confirm Password:</label>
+            <div className="border-1 flex items-center rounded-[5px] w-[95%] focus-within:border-2 bg-white">
+              <input
+                type={showCurrentPassword ? "text" : "password"}
+                placeholder="Current Password"
+                className="px-1 rounded-[5px] w-full bg-transparent outline-none"
+                {...register("confirmPassword")}
+              />
+              <FontAwesomeIcon
+                icon={showCurrentPassword ? faEyeSlash : faEye}
+                color="gray"
+                className="cursor-pointer mr-1"
+                onClick={() => setShowCurrentPassword(!showCurrentPassword)}
+              />
+            </div>
+            {errors.confirmPassword && (
+              <span className="text-red-500 text-xs">
+                {errors.confirmPassword.message}
+              </span>
+            )}
+          </>
+
+          <>
+            <button
+              type="submit"
+              className="bg-[#A7C1A8] text-white p-1 rounded-[5px] mt-3 w-full cursor-pointer"
+            >
+              Register
+            </button>
+          </>
         </form>
         <a href="/" className="mt-3 underline text-[#A7C1A8]">
           To login Click here.
